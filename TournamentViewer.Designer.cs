9a2acd9411d9a5213246293f43e44ded18b608b6
Jennifer Downes <genniferdee@yahoo.ie>
2022-10-30 11:53:11 +0000

    Main Tournament Ui

--- TournamentViewer.Designer.cs
+++ TournamentViewer.Designer.cs
@@ -29,37 +29,142 @@ namespace simpleTracker
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.matchUpListBox = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RoundSelect = new System.Windows.Forms.ComboBox();
+            this.chkUnplayed = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // matchUpListBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(626, 60);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(341, 516);
-            this.listBox1.TabIndex = 0;
+            this.matchUpListBox.FormattingEnabled = true;
+            this.matchUpListBox.ItemHeight = 16;
+            this.matchUpListBox.Location = new System.Drawing.Point(22, 150);
+            this.matchUpListBox.Name = "matchUpListBox";
+            this.matchUpListBox.Size = new System.Drawing.Size(422, 340);
+            this.matchUpListBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(79, 60);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(385, 95);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(279, 28);
+            this.label1.Size = new System.Drawing.Size(162, 28);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Tournament: <Name>";
+            this.label1.Text = "Tournament: ";
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(390, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "<name>";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(55, 551);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Round ";
+            // 
+            // RoundSelect
+            // 
+            this.RoundSelect.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoundSelect.FormattingEnabled = true;
+            this.RoundSelect.ItemHeight = 21;
+            this.RoundSelect.Location = new System.Drawing.Point(187, 550);
+            this.RoundSelect.Name = "RoundSelect";
+            this.RoundSelect.Size = new System.Drawing.Size(170, 29);
+            this.RoundSelect.TabIndex = 4;
+            // 
+            // chkUnplayed
+            // 
+            this.chkUnplayed.AutoSize = true;
+            this.chkUnplayed.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUnplayed.ForeColor = System.Drawing.Color.DarkRed;
+            this.chkUnplayed.Location = new System.Drawing.Point(196, 607);
+            this.chkUnplayed.Name = "chkUnplayed";
+            this.chkUnplayed.Size = new System.Drawing.Size(152, 23);
+            this.chkUnplayed.TabIndex = 5;
+            this.chkUnplayed.Text = "Unplayed Only";
+            this.chkUnplayed.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(490, 410);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Points";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(494, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Points";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(467, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 37);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "<name>";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkRed;
+            this.label7.Location = new System.Drawing.Point(467, 359);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(138, 37);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "<name>";
+            // 
+            // TournamentViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1010, 722);
+            this.ClientSize = new System.Drawing.Size(891, 722);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkUnplayed);
+            this.Controls.Add(this.RoundSelect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
-            this.Name = "Form1";
+            this.Controls.Add(this.matchUpListBox);
+            this.Name = "TournamentViewer";
             this.Text = "Tournament";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -69,8 +174,16 @@ namespace simpleTracker
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox matchUpListBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox RoundSelect;
+        private System.Windows.Forms.CheckBox chkUnplayed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
