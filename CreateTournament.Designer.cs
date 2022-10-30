a68bb32fc4ada0036b536ad2a46dada81dd374f9
Jennifer Downes <genniferdee@yahoo.ie>
2022-10-30 16:35:54 +0000

    Create tournament Ui

---
+++ CreateTournament.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace simpleTracker
+{
+    partial class CreateTournament
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tournamentName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.createNewTeamLink = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectedTeams = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.deleteTeam = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Create New Tournament: ";
+            // 
+            // tournamentName
+            // 
+            this.tournamentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tournamentName.Location = new System.Drawing.Point(51, 146);
+            this.tournamentName.Name = "tournamentName";
+            this.tournamentName.Size = new System.Drawing.Size(345, 34);
+            this.tournamentName.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(46, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(241, 28);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tournament Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(46, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 28);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Entry Fee:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(51, 234);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(345, 34);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(46, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 28);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Select Team";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(51, 323);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(345, 35);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // createNewTeamLink
+            // 
+            this.createNewTeamLink.AutoSize = true;
+            this.createNewTeamLink.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createNewTeamLink.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.createNewTeamLink.Location = new System.Drawing.Point(232, 371);
+            this.createNewTeamLink.Name = "createNewTeamLink";
+            this.createNewTeamLink.Size = new System.Drawing.Size(164, 21);
+            this.createNewTeamLink.TabIndex = 18;
+            this.createNewTeamLink.TabStop = true;
+            this.createNewTeamLink.Text = "Create New Team";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(119, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 52);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Add Team ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // selectedTeams
+            // 
+            this.selectedTeams.FormattingEnabled = true;
+            this.selectedTeams.ItemHeight = 16;
+            this.selectedTeams.Location = new System.Drawing.Point(478, 146);
+            this.selectedTeams.Name = "selectedTeams";
+            this.selectedTeams.Size = new System.Drawing.Size(335, 276);
+            this.selectedTeams.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(473, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 28);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Teams";
+            // 
+            // deleteTeam
+            // 
+            this.deleteTeam.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteTeam.ForeColor = System.Drawing.Color.DarkRed;
+            this.deleteTeam.Location = new System.Drawing.Point(563, 454);
+            this.deleteTeam.Name = "deleteTeam";
+            this.deleteTeam.Size = new System.Drawing.Size(182, 52);
+            this.deleteTeam.TabIndex = 22;
+            this.deleteTeam.Text = "Delete";
+            this.deleteTeam.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkRed;
+            this.button3.Location = new System.Drawing.Point(255, 546);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(317, 52);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Create Tournament";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // CreateTournament
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 626);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.deleteTeam);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.selectedTeams);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.createNewTeamLink);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tournamentName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateTournament";
+            this.Text = "Create Tournament";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tournamentName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.LinkLabel createNewTeamLink;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox selectedTeams;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button deleteTeam;
+        private System.Windows.Forms.Button button3;
+    }
+}
